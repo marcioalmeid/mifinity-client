a07c8a3fd74776f4467453edbc6d152bf57f6057
marcioalmeid <marcioalmeid@gmail.com>
2018-05-31 20:03:27 +0100

    Edit User

--- src/app/user-edit/user-edit.component.ts
+++ src/app/user-edit/user-edit.component.ts
@@ -1,15 +1,56 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnDestroy, OnInit } from '@angular/core';
+import { ActivatedRoute, Router } from '@angular/router';
+import { UserService } from '../shared/user/user.service';
+import { NgForm } from '@angular/forms';
 
 @Component({
   selector: 'app-user-edit',
   templateUrl: './user-edit.component.html',
   styleUrls: ['./user-edit.component.css']
 })
-export class UserEditComponent implements OnInit {
+export class UserEditComponent implements OnInit, OnDestroy {
+  user: any = {};
 
-  constructor() { }
+  
 
-  ngOnInit() {
+  constructor(private route: ActivatedRoute,
+              private router: Router,
+              private userService: UserService) {
   }
 
+  ngOnInit() {
+     this.route.params.takeUntil(componentDestroyed(this)).subscribe(params => {
+      const id = params['id'];
+      if (id) {
+        this.userService.get(id).subscribe((user: any) => {
+          if (user) {
+            this.user = user;
+            this.user.href = user._links.self.href;            
+          } else {
+            console.log(`User with id '${id}' not found, returning to list`);
+            this.gotoList();
+          }
+        });
+      }
+    });
+  }
+
+  ngOnDestroy() {
+  }
+
+  gotoList() {
+    this.router.navigate(['/user-list']);
+  }
+
+  save(form: NgForm) {
+    this.userService.save(form).subscribe(result => {
+      this.gotoList();
+    }, error => console.error(error));
+  }
+
+  remove(href) {
+    this.userService.remove(href).subscribe(result => {
+      this.gotoList();
+    }, error => console.error(error));
+  }
 }
