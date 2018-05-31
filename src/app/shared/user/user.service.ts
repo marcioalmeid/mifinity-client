0051671b33672bb8f53eabe2a3c87d68914f3677
marcioalmeid <marcioalmeid@gmail.com>
2018-05-31 19:02:56 +0100

    fixes

--- src/app/shared/user/user.service.ts
+++ src/app/shared/user/user.service.ts
@@ -7,8 +7,30 @@ export class UserService {
 
   constructor(private http: HttpClient) {
   }
+  
+  public API = 'https://minfinity.cfapps.io/';
+  public USER_API = this.API + '/user';
 
   getAll(): Observable<any> {
-    return this.http.get('https://minfinity.cfapps.io/home/all');
+    return this.http.get(this.API + '/list');
   }
+
+  get(id: string) {
+    return this.http.get(this.USER_API + '/' + id);
+  }
+
+  save(user: any): Observable<any> {
+    let result: Observable<Object>;
+    if (user['userName']) {
+      result = this.http.put(user.userName, user);
+    } else {
+      result = this.http.post(this.USER_API, user);
+    }
+    return result;
+  }
+
+  remove(userName: string) {
+    return this.http.delete(userName);
+  }
+
 }
