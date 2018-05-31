a67583186309795da861d2d0a5c49a1f3317507a
marcioalmeid <marcioalmeid@gmail.com>
2018-05-31 17:51:55 +0000

    user edit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,11 +6,15 @@ import { AppComponent } from './app.component';
 import { UserListComponent } from './user-list/user-list.component';
 import { MatButtonModule, MatCardModule, MatInputModule, MatListModule, MatToolbarModule } from '@angular/material';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { CarEditComponent } from './car-edit/car-edit.component';
+import { UserEditComponent } from './user-edit/user-edit.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    UserListComponent
+    UserListComponent,
+    CarEditComponent,
+    UserEditComponent
   ],
   imports: [
     BrowserModule,
