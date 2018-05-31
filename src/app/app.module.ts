e82a2f4da5342c7165fb4b4c00895cc8b38b1e2a
marcioalmeid <marcioalmeid@gmail.com>
2018-05-31 14:51:13 +0000

    Angular 6 client for MiFinity application

---
+++ src/app/app.module.ts
@@ -0,0 +1,20 @@
+import { UserService } from './shared/user/user.service';
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+import { HttpClientModule } from '@angular/common/http';
+import { AppComponent } from './app.component';
+import { UserListComponent } from './user-list/user-list.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    UserListComponent
+  ],
+  imports: [
+    BrowserModule,
+    HttpClientModule
+  ],
+  providers: [UserService],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
